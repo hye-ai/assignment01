b5c1e93ee3857f9eeb1b84d25a450be4ee654de7
hye-ai <nctwio78@gmail.com>
2024-10-18 23:37:53 +0900

    Update Lab 1-1.py

--- src/Lab 1-1.py
+++ src/Lab 1-1.py
@@ -1,22 +1,23 @@
-# Selection sorting (최소값 중심 정리)
+# Selection sorting (최소값 중심 정렬)
 def selectionSort(list):
 
-    # Traverse through all array elements
+    # 배열의 모든 요소를 순회
     for i in range(len(list)):
 
-        # Find the minimum element in remaining unsorted array
+        # 남은 미정렬 배열에서 최소값 찾기
         min_idx = i
         for j in range(i+1, len(list)):
             if list[min_idx] > list[j]:
                 min_idx = j
-        # Swap the found minimum element with the first element
+
+        # 찾은 최소값을 첫 번째 요소와 교체
         list[i], list[min_idx] = list[min_idx], list[i]
 
-# Driver code to test above
+# 실행 코드
 list = [64, 25, 12, 22, 11]
-print("Original list = %s" % list)
+print("Original list: %s" % list)
 
 selectionSort(list)
 
-print("Sorted list by Selection method")
+print("Sorted list by Selection Sort method:")
 print(list)
